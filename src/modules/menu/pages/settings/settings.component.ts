f511abb5beff55e09507ab91365d53a48d7af7d8
shumeikov <viktor.shumeiko@simbirsoft.com>
2024-09-07 19:56:35 +0300

    add settings page

--- src/modules/menu/pages/settings/settings.component.ts
+++ src/modules/menu/pages/settings/settings.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {SocketService} from "@game/services/socket.service";
+import {NAKAMA} from "@api/nakama";
+import {Match} from "@heroiclabs/nakama-js";
+import {SessionService} from "@auth/services/session.service";
+import {FormsModule} from "@angular/forms";
+
 
 @Component({
-    templateUrl: './settings.component.html',
-    styleUrl: './settings.component.scss'
+  selector: 'file-upload',
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.scss',
+  imports: [
+    FormsModule
+  ],
+  standalone: true
 })
 export default class SettingsPageComponent {
-    
+  fileName = '';
+  private socket = inject(SocketService);
+  private nakama = inject(NAKAMA);
+  private session = inject(SessionService);
+  // constructor(private http: HttpClient) {}
+  username: string = '';
+
+  onFileSelected(event:any) {
+    // this.socket.socket!.
+
+    const file:File = event.target.files[0];
+
+    if (file) {
+
+      this.fileName = 'Выбран: '+file.name;
+
+      // const formData = new FormData();
+      //
+      // formData.append("thumbnail", file);
+      //
+      // const upload$ = this.http.post("/api/thumbnail-upload", formData);
+      //
+      // upload$.subscribe();
+    }
+  }
+  save() {
+    this.nakama.updateAccount(this.session.session!, {
+      display_name : this.username,
+      avatar_url: this.fileName,
+      username: this.username
+    });
+  }
 }
